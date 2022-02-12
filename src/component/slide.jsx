1923f0e4d98e570caded12746b4b66c8956ba199
jose zambrano <joseyzambranovpe@gmail.com>
2022-02-11 20:56:07 -0500

    finish slider

--- src/component/slide.jsx
+++ src/component/slide.jsx
@@ -1,8 +1,11 @@
 import styled from "styled-components"
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import {data} from '../data';
+import { useState } from "react";
 
 const Container = styled.div`
+overflow:hidden;
 width:100%;
 height:100vh;
 position:relative;
@@ -10,11 +13,11 @@ display:flex;
 `
 
 const Wrapper = styled.div`
-width:100vw;
-height:100vh;
-background-color:#f3356b;
+
+height:100%;
 display:flex;
-transform:translateX()
+transition: all 1.5s ease;
+transform:translateX(${(props)=>props.sliderIndex * -100}vw);
 `
 const Arrow = styled.div`
 width:50px;
@@ -31,19 +34,21 @@ bottom:0;
 position:absolute;
 left:${props=>props.position === "left"&&"10px"};
 right:${props=>props.position === "right"&&"10px"};
+z-index:2;
 `
 const Slides = styled.div`
 width:100vw;
 height:100vh;
 display:flex;
 align-items:center;
+background-color:#${(props)=>props.bg};
 `
 const ImgContainer = styled.div`
 height:100%;
 flex:1;
 `
 const Img = styled.img`
-height:100%;
+height:90%;
 
 `
 const InfoContainer = styled.div`
@@ -69,48 +74,42 @@ color:white;
 `
 
 const Slide =()=>{
+
+    const[sliderIndex,setSliderIndex]=useState(0)
+
+    const handleClick = (position)=>{
+        if(position==="left"){
+            setSliderIndex(sliderIndex > 0? sliderIndex -1:3)
+        }else{
+            setSliderIndex(sliderIndex < 3? sliderIndex +1:0)
+        }
+    }
+ 
     return(
         <Container>
-            <Arrow position="left">
+            <Arrow position="left" onClick={()=>handleClick("left")}>
             <ArrowBackIosNewIcon/>
             </Arrow>
-            <Wrapper>
-                <Slides>
-                    <ImgContainer>
-                    <Img src="https://i.postimg.cc/J4b7zbCb/sincerely-media-uu-Rw-SS1hx-Q-unsplash-removebg-preview.png"/>
-                    </ImgContainer>
-                    <InfoContainer>
-                    <Title>Lorem Ipsum</Title>
-                    <Desc>is simply dummy text of the printing and typesetting industry.</Desc>
-                    <Button>Buy now</Button>
-                    </InfoContainer>
-                </Slides>
-            </Wrapper>
-            <Wrapper>
-                <Slides>
-                    <ImgContainer>
-                    <Img src="https://i.postimg.cc/J4b7zbCb/sincerely-media-uu-Rw-SS1hx-Q-unsplash-removebg-preview.png"/>
-                    </ImgContainer>
-                    <InfoContainer>
-                    <Title>Lorem Ipsum 2</Title>
-                    <Desc>is simply dummy text of the printing and typesetting industry.</Desc>
-                    <Button>Buy now</Button>
-                    </InfoContainer>
-                </Slides>
-            </Wrapper><Wrapper>
-                <Slides>
-                    <ImgContainer>
-                    <Img src="https://i.postimg.cc/J4b7zbCb/sincerely-media-uu-Rw-SS1hx-Q-unsplash-removebg-preview.png"/>
-                    </ImgContainer>
-                    <InfoContainer>
-                    <Title>Lorem Ipsum 3</Title>
-                    <Desc>is simply dummy text of the printing and typesetting industry.</Desc>
-                    <Button>Buy now</Button>
-                    </InfoContainer>
-                </Slides>
-            </Wrapper>
+            <Wrapper sliderIndex={sliderIndex}>
+                {data.map((data)=>(
 
-            <Arrow position="right">
+                        <Slides bg={data.bg} key={data.id}>
+                        <ImgContainer>
+                        <Img src={data.img}/>
+                        </ImgContainer>
+                        <InfoContainer>
+                        <Title>{data.title}</Title>
+                        <Desc>{data.desc}</Desc>
+                        <Button>Buy now</Button>
+                        </InfoContainer>
+                        </Slides>
+
+                ))
+                    
+
+                }
+            </Wrapper>
+            <Arrow position="right" onClick={()=>handleClick("right")}>
             <ArrowForwardIosIcon/>
             </Arrow>
         </Container>
---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const data = [{
+        id: 1,
+        img: "https://i.postimg.cc/J4b7zbCb/sincerely-media-uu-Rw-SS1hx-Q-unsplash-removebg-preview.png",
+        desc: "is simply dummy text of the printing and typesetting industry.",
+        title: "Lorem Ipsum",
+        bg: "f3356b"
+    },
+    {
+        id: 2,
+        img: "https://i.postimg.cc/J4b7zbCb/sincerely-media-uu-Rw-SS1hx-Q-unsplash-removebg-preview.png",
+        desc: "is simply dummy text of the printing and typesetting industry.",
+        title: "Lorem Ipsum 2",
+        bg: "f3356b"
+    },
+    {
+        id: 3,
+        img: "https://i.postimg.cc/J4b7zbCb/sincerely-media-uu-Rw-SS1hx-Q-unsplash-removebg-preview.png",
+        desc: "is simply dummy text of the printing and typesetting industry.",
+        title: "Lorem Ipsum 3",
+        bg: "f3356b"
+    },
+    {
+        id: 4,
+        img: "https://i.postimg.cc/J4b7zbCb/sincerely-media-uu-Rw-SS1hx-Q-unsplash-removebg-preview.png",
+        desc: "is simply dummy text of the printing and typesetting industry.",
+        title: "Lorem Ipsum 4",
+        bg: "f3356b"
+    }
+
+]
